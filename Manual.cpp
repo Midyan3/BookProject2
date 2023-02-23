ad70c2c213efd388692bbea72aec2e87c3f3039d
Midyan3 <39417120+Midyan3@users.noreply.github.com>
2023-02-23 19:45:00 +0000

    final

--- Manual.cpp
+++ Manual.cpp
@@ -1,8 +1,27 @@
 #include "Manual.hpp"
 #include <regex>
-
+/**
+ * @brief Construct a new Manual:: Manual object
+ * Default constructor.
+  Default-initializes all private members. 
+  Booleans are default-initialized to False
+ * 
+ */
 Manual::Manual()
     :  url_(""), device_name_(""), has_website_(false), visual_aid_(false) { }
+
+
+/**
+ * @brief Construct a new Manual:: Manual object. initialize the private members
+ * 
+ * @param title 
+ * @param author 
+ * @param page_count 
+ * @param device_name 
+ * @param is_digital 
+ * @param url 
+ * @param visual_aid 
+ */
 
 Manual::Manual(const std::string title, const std::string author, int page_count, const std::string device_name,bool is_digital, const std::string url,bool visual_aid)
     : Book(title, author, page_count, is_digital)
@@ -19,29 +38,51 @@ Manual::Manual(const std::string title, const std::string author, int page_count
    
     
 } 
+/**
+    @param  : a reference to a std::string representing the device
+    @post   : sets the private member variable to the value of the parameter
+**/
 
 void Manual::setDevice(const std::string& device_name){
     device_name_ = device_name;
 }
+/**
+  @return  : the visual aid flag
+**/
 bool Manual::hasVisualAid() const{
     return visual_aid_;
 }
 bool Manual::hasWebsite() const{
     return has_website_;
 }
+
+/**
+    @return  : the url for the website
+**/
 std::string Manual::getWebsite() const{
     return url_;
 }
+/**
+    @return  : the variable indicating the device the manual is for
+**/
 std::string Manual::getDevice() const{
     return device_name_;
 }
+/**
+     @return  : has_website_ flag (a boolean) 
+ */
 bool Manual::setWebsite(const std::string& website_name){
     has_website_ = true;
     url_ = "Broken Link";
     if(URLhelper(website_name)) url_ = website_name;
     return has_website_;
 }
-void Manual::setVisualAid(bool new_has_aids){
+/**
+    @param  : a reference to boolean
+    @post   : sets the private member variable indicating the presence
+            of a visual aid to the value of the parameter
+**/
+void Manual::setVisualAid(const bool new_has_aids){
     visual_aid_ = new_has_aids;
 }
 bool Manual::URLhelper(const std::string& url){
--- Manual.hpp
+++ Manual.hpp
@@ -6,15 +6,83 @@
 
 class Manual: public Book{
 public:
+/**
+    Default constructor.
+    Default-initializes all private members.
+*/
     Manual();
+/**
+  Parameterized constructor.
+    @param     : The title of the book (a std::string)
+    @param     : The author of the book (a std::string)
+    @param     : The number of pages in the book (a positive integer)
+    @param     : A flag indicating whether the book is in digital form
+                with DEFAULT VALUE False (a Boolean)
+    @param     : The name of the device (a std::string)
+    @param     : The URL that is in the format
+                'https://www.something.something'
+                or 'http://www.something.something'
+                with the last 'something' being at least 2 characters
+                with DEFAULT VALUE empty std::string (a std::string)
+    @param     : A Boolean indicating the presence of a visual aid
+                with DEFAULT VALUE False
+    @post      : The private members are set to the values of the
+                corresponding parameters.
+                If a URL is provided, the website flag is set to True.
+                If the URL is ill-formatted, the website is set to
+                empty std::string and the website flag is set to False.
+*/
     Manual(const std::string title, const std::string author, int page_count, const std::string device_name,bool is_digital = false, const std::string url = "",bool visual_aid = false);
+    /**
+     * @brief Set the Device object
+     * 
+     * @param device_name : the name of the device thats being used
+     */
     void setDevice(const std::string& device_name);
+    /**
+     * @brief Gets the name of Device object
+     * 
+     * @return std::string of the device object 
+     */
     std::string getDevice() const;
+    /**
+     * @brief Set the Website object if the url matches the requirments else it sets string url to "Broken Link"
+     * 
+     * @param website_name: Url of website 
+     * @return true 
+     */
     bool setWebsite(const std::string& website_name);
-    void setVisualAid(bool new_has_aids);
+    /**
+     * @brief Set the Visual Aid object to either true or false depening on the param
+     * 
+     * @param new_has_aids 
+     */
+    void setVisualAid(const bool new_has_aids);
+    /**
+     * @brief Get the Website object
+     * 
+     * @return std::string 
+     */
     std::string getWebsite() const;
+    /**
+     * @brief returns true if it the book has viusal aids or false if it doesn't
+     * 
+     * @return true or false 
+     */
     bool hasVisualAid() const;
+    /**
+     * @brief Returns wether it contains a website or not
+     * 
+     * @return true or false
+     */
     bool hasWebsite() const;
+    /**
+     * @brief Returns true if the website is formatted the right way orr false if it isn't
+     * 
+     * @param url 
+     * @return true 
+     * @return false 
+     */
     bool URLhelper(const std::string& url);
 
 private:
